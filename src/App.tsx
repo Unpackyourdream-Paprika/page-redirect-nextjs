6a64da849f74d0192914709bcb7f60dd0b7e3d2c
s2s2hyun <s2s2hyun0601@gmail.com>
2025-09-04 18:09:40 +0900

    Fix url

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 function App() {
   useEffect(() => {
-    window.location.href = "https://naver.com";
+    window.location.href = "https://nanobanana-google.com/";
   }, []);
 
   return null; // 화면은 비워둬도 됨
