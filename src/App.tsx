09742474850e797646af786884b9b244253b16d1
s2s2hyun <s2s2hyun0601@gmail.com>
2025-09-04 18:21:49 +0900

    [FF;2C]

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 function App() {
   useEffect(() => {
-    window.location.href = "https://nanobananas.store/";
+    window.location.href = "https://www.nanobananas.store/";
   }, []);
 
   return null; // 화면은 비워둬도 됨
